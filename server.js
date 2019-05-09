97a64ba0a10020f9ca48d74a02cc4bcee312873c
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-05-09 17:27:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+
+    const dbs = database.db("test")
+    require('./app/routes')(app, dbs);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
