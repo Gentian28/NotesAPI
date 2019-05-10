ab9b8df373cb4fe95dc3446a69f97bcd2870f067
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-05-10 12:23:13 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: "mongodb://localhost:27017/test"
-    url: "mongodb+srv://root:root@cluster0-6omxc.mongodb.net/test?retryWrites=true"
+    url: "mongodb://localhost:27017/test"
 };
