a77e672bd4c3fb89b869f7d425cfdbeabbf54681
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-05-10 12:03:58 +0200

    MongoDB Atlas server

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/test"
+    // url: "mongodb://localhost:27017/test"
+    url: "mongodb+srv://root:root@cluster0-6omxc.mongodb.net/test?retryWrites=true"
 };
